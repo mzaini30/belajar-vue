9d756b98f3ee071ef3195796a93c824b1ca7aebe
mzaini30 <muhzaini30@gmail.com>
2021-06-28 01:39:11 +0800

    upload

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ import { createApp } from 'vue/dist/vue.esm-bundler'
 import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import {routes} from './routes'
-// import VueMeta from 'vue-meta'
 
 const router = createRouter({
 	routes,
 	history: createWebHistory()
 })
 
-createApp(App).use(router)/*.use(VueMeta)*/.mount('#app')
+createApp(App).use(router).mount('#app')
